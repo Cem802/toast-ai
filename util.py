ec0a2bf90cdb2efbf635b0cdcb031460649c96a1
Cem802 <ckirciltepeli@gmail.com>
2024-02-28 18:12:06 +0100

    Add StackFrontier class to util.py

--- util.py
+++ util.py
@@ -25,3 +25,11 @@ class QueueFrontier():
             node = self.frontier[0]
             self.frontier = self.frontier[1:]
             return node
+class StackFrontier():
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
